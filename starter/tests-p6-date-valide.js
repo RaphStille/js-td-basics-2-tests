df98f1ba4c7873eb3852594193c99e1e4bea8d08
RaphStille <raphael.stille@student.hepl.be>
2024-10-09 11:21:46 +0200

    Update tests-p6-date-valide.js

--- starter/tests-p6-date-valide.js
+++ starter/tests-p6-date-valide.js
@@ -1,56 +1,32 @@
-/*
-TESTS - PRÉPA 6 : Date valide
-
-Écrire un programme JS qui teste si une date entrée par l’utilisateur est une date valide ou pas.
-
-AIDE (en français) :
-Pour qu’une date soit valide, il faut que la date du jour ne dépasse pas
-le nombre de jours maximum autorisé par mois, c.-à-d. 30 ou 31 selon le mois
-et, pour le mois de février : 28 si l’année est normale, 29 si l’année est bissextile.
-
-Étapes :
-1. Demandez à l'utilisateur d'entrer une date sous forme de jour, mois et année, puis stockez ces valeurs dans des variables (par exemple, day, month, year).
-2. Initialisez une variable maxDays pour stocker le nombre maximum de jours autorisés pour le mois.
-3. Utilisez une série de conditions pour déterminer maxDays en fonction du mois (y compris le cas de février avec gestion bissextile).
-4. Utilisez une autre condition pour vérifier si la date est valide en comparant le jour entré par l'utilisateur avec maxDays.
-5. Affichez dans la console si la date est valide ou non : "La date … est valide" ou "La date … n'est PAS valide".
-
-1* Demandez année, mois et jours et les stocker
-2* testez si ce sont des nombres et s'ils sont positifs
-3* Verifiez que le nombre de jour entré ne depasse pas 30 ou 31 (28 pour février et 29 février bissextile) selon le mois
-4* verifiez la validité de la date
-5* Notifiez dans la console si elle l'est ou pas
-*/
-
-let year = prompt("Quelle année?")
-let month = prompt("Quel mois?")
-let day = prompt("Quel jour?")
-
-let isBissextile = false;
-if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
-    isBissextile = true;
-}
-
+let year = parseInt(prompt("Quelle année?"));
+let month = parseInt(prompt("Quel mois?"));
+let day = parseInt(prompt("Quel jour?"));
+let isValid = false;
 let maxDays = 31;
 
-if (month === "04" ||month === "06" ||month === "09" ||month === "11") {
+if (month === 4 || month === 6 || month === 9 || month === 11) {
     maxDays = 30;
-}
-if (month === "02") {
+} else if (month === 2) {
     maxDays = 28;
-}
-if (month === "02" && isBissextile === true) {
-    maxDays = 29;
-} else {
-    console.log("Erreur dans le mois");
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        maxDays = 29;
+    }
 }
 
-if (day < maxDays) {
-    if (month <= 12) {
-        console.log(`${day}/${month}/${year} est une date valide avec un mois en ${maxDays}jours`);
+if (year && month && day) {
+    if (day > 0 && month > 0 && year > 0) {
+        if (month <= 12) {
+            if (day <= maxDays) {
+                console.log(`${day}/${month}/${year} est une date valide.`);
+            } else {
+                console.log("Jour superieur au nombre du jour dans le mois");
+            }
+        } else {
+            console.log("Mois superieur à 12");
+        }
+    } else {
+        console.log("Jour, mois & année doivent etre un nombre positif");
     }
 } else {
-    console.log(`Le nombre de jour que vous avez cité n'existe pas dans le mois`);
+    console.log("Entrez un NOMBRE svp.");
 }
-
-
